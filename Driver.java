e0b1e23f9f20b97263ac006b60874115462bec82
Lyam-Katz <lyamkatzuniversity@gmail.com>
2023-06-11 21:17:22 -0400

    test

--- Driver.java
+++ Driver.java
@@ -1,16 +1,16 @@
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+		double[] coefficients = {1,2,3};
+		int[] powers = {1,2,3};
+		Polynomial p = new Polynomial(coefficients, powers);
+		for(int i = 0; i<5; i++){
+			System.out.println(p.multiply(p).coefficients[i]);
+
+		}
+		for(int i = 0; i<5; i++){
+                        System.out.println(p.multiply(p).exponents[i]);
+
+                }
+		System.out.println(p.multiply(p).exponents.length);
 	}
 }
